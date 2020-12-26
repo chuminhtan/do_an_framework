b7ccd857fb9559c3c3f2fcb738f6f49a59691348
VyUIT <18520403@gm.uit.edu.vn>
2020-12-26 23:26:22 +0700

    update order

--- do_an_framework/Startup.cs
+++ do_an_framework/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using do_an_framework.Models;
 
 namespace do_an_framework
 {
@@ -23,6 +24,8 @@ namespace do_an_framework
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<MySqlDatabase>(_ => new MySqlDatabase("server=localhost; database=cake_db; uid=root; pwd=;"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
